02d995531b393134e2908cbe6da5980988dbf0ac
Ryan Weeks <weeksrm8@gmail.com>
2026-02-27 12:34:21 -0500

    Updated Sanity Again

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,19 +1,16 @@
-import gallery from './gallery'
 import floorplan from './floorplan'
 
-import gallery from './gallery'
+
 import floorplan from './floorplan'
 import property from './property'
-import floor from './floor'
+
 import PortfolioPage from './PortfolioPage'
 import vacanciesPage from '../schemas/vacanciesPage'
 
 
 export const schemaTypes = [
   property,
-  gallery,
   floorplan,
-  floor,
   PortfolioPage,
   vacanciesPage,
   
--- schemas/schema.js
+++ schemas/schema.js
@@ -1,21 +1,17 @@
 // schemas/schema.js
-import teaser from './teaser';
-import amenities from './amenities'
-import gallery from './gallery'
+
 import floorplan from './floorplan'
 import property from './property'
-import floor from './floor'
+
 import PortfolioPage from './PortfolioPage'
 import vacanciesPage from './vacanciesPage';
 
 
 export const schemaTypes = [
-  teaser,
-  amenities,
-  gallery,
+ 
   floorplan,
   property,
-  floor,
+ 
   PortfolioPage,
   vacanciesPage,
 
